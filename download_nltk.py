f2832bb88e9e799be1cef4d7ab322e736d3cfce9
Samradni Dahiphale <samradnidahiphale@gmail.com>
2026-08-12 20:25:04 +0530

    NLP

---
+++ download_nltk.py
@@ -0,0 +1,12 @@
+import nltk
+
+# Download all required NLTK resources
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
+
+print("All NLTK resources downloaded successfully!")
